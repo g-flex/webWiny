26d051393333ca8341690370aa79c30736293edf
g-flex <ggussoni97@gmail.com>
2019-03-29 04:16:51 +0100

    banner-animazioni-scroll

---
+++ js/bandi.js
@@ -0,0 +1,34 @@
+const $ = (selector) => document.querySelector(selector);
+
+let offset = 800;
+let call;
+function scroll() {
+    if ((offset - document.documentElement.scrollTop) > 20) {
+        document.documentElement.scrollTop += 20
+    } else if ((offset - document.documentElement.scrollTop) < -20) {
+        document.documentElement.scrollTop -= 20
+    } else {
+        clearInterval(call)
+    }
+};
+
+$('#scopri-bando').addEventListener("click", reply_click);
+
+function reply_click() {
+    event.preventDefault();
+    call = setInterval(scroll, 10);
+}
+var isInViewport = function (elem) {
+    var bounding = elem.getBoundingClientRect();
+    return (
+        bounding.top >= 0 &&
+        bounding.bottom <= (window.innerHeight || document.documentElement.clientHeight)
+    );
+};
+window.addEventListener('scroll', function (event) {
+	if (isInViewport($('.info-bando-1>h3'))) {
+		document.querySelectorAll('.info-bando').forEach((info)=>{
+			info.classList.remove('margin-opacity-out');
+		});
+	}
+}, false);
--- js/main.js
+++ js/main.js
@@ -1,90 +1,4 @@
 const $ = (selector) => document.querySelector(selector);
-let currentCarouselPage = 0;
-
-$('.carousel__window').style.left=0;
-$('.crumb-0').style='height: 7px; background-color: #2b0116;';
-function clickPrevious(){
-	if(currentCarouselPage == 0){
-		$('.carousel__window').style.left='-200vw';
-		$('.crumb-2').style='height: 7px; background-color: #2b0116;';
-		$('.crumb-0').style='';
-		$('.crumb-1').style='';
-		currentCarouselPage = 2;
-	} else if(currentCarouselPage == 1){
-		$('.carousel__window').style.left=0;
-		$('.crumb-2').style='';
-		$('.crumb-0').style='height: 7px; background-color: #2b0116;';
-		$('.crumb-1').style='';
-		currentCarouselPage = 0;
-	} else if(currentCarouselPage == 2){
-		$('.carousel__window').style.left='-100vw';
-		$('.crumb-2').style='';
-		$('.crumb-0').style='';
-		$('.crumb-1').style='height: 7px; background-color: #2b0116;';
-		currentCarouselPage = 1;
-	}
-}
-function clickNext(){
-	if(currentCarouselPage == 0){
-		$('.carousel__window').style.left='-100vw';
-		$('.crumb-2').style='';
-		$('.crumb-0').style='';
-		$('.crumb-1').style='height: 7px; background-color: #2b0116;';
-		currentCarouselPage = 1;
-	} else if(currentCarouselPage == 1){
-		$('.carousel__window').style.left='-200vw';
-		$('.crumb-2').style='height: 7px; background-color: #2b0116;';
-		$('.crumb-0').style='';
-		$('.crumb-1').style='';
-		currentCarouselPage = 2;
-	} else if(currentCarouselPage == 2){
-		$('.carousel__window').style.left=0;
-		$('.crumb-2').style='';
-		$('.crumb-0').style='height: 7px; background-color: #2b0116;';
-		$('.crumb-1').style='';
-		currentCarouselPage = 0;
-	}
-}
-$('.crumb-0').addEventListener('click', function(){
-	if(currentCarouselPage == 1){
-		clickPrevious();
-	} else if(currentCarouselPage == 2){
-		clickNext();
-	}
-	currentCarouselPage = 0;
-});
-$('.crumb-1').addEventListener('click', function(){
-	if(currentCarouselPage == 2){
-		clickPrevious();
-	} else if(currentCarouselPage == 0){
-		clickNext();
-	}
-	currentCarouselPage = 1;
-});
-$('.crumb-2').addEventListener('click', function(){
-	if(currentCarouselPage == 0){
-		clickPrevious();
-	} else if(currentCarouselPage == 1){
-		clickNext();
-	}
-	currentCarouselPage = 2;
-});
-
-document.onkeydown = function(e) {
-	e = e || window.event;
-	switch(e.which || e.keyCode) {
-		case 37: clickPrevious();
-		break;
-		case 39: clickNext();
-		break;
-		default: return;
-	}
-	e.preventDefault();
-}
-
-$('.previous').addEventListener('click', clickPrevious);
-$('.next').addEventListener('click', clickNext);
-
 
 var isInViewport = function (elem) {
     var bounding = elem.getBoundingClientRect();
@@ -109,3 +23,8 @@ window.addEventListener('scroll', function (event) {
 		$('.illustration p').style.right='50px';
 	}
 }, false);
+
+document.addEventListener("DOMContentLoaded", () => {
+	console.log('loaded');
+	$('#banner').play();
+});
